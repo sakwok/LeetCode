cdad1edc801252110bab276e92d4113a29e9a3c1
sakwok <s3.kwok@gmail.com>
2018-11-03 17:38:05 -0700

    first not repeating char

---
+++ firstNotRepeatingChar.js
@@ -0,0 +1,9 @@
+function firstNotRepeatingCharacter(s) {
+  let result = '_';
+  for (let i = 0; i < s.length; i++) {
+      if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) {
+          return s[i];
+      }
+  }
+  return result;
+}
