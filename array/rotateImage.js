7a52758920b5515e6ef310765f90f6e0aab5f24b
sakwok <s3.kwok@gmail.com>
2018-11-06 16:10:56 -0800

    rotateImage

--- array/rotateImage.js
+++ array/rotateImage.js
@@ -1,27 +1,13 @@
 function rotateImage(a) {
-  let half = Math.floor(arr[0].length / 2);
-  let holder = 0;
-
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < half; j++) {
-      holder = a[i][j];
-      a[i][j] = a[i][a[i].length - 1 - j];
-      a[i][a[i].length - 1 - j] = holder;
-    }
-  }
-
-  for (let x = -(a.length - 1); x <= 0; x++) {
-    for (let y = 0; y < a.length; y++) {
-      if (a[x][y]) {
-        holder = a[x][y];
-
+  for (let i = 0; i < Math.floor(a.length/2); i++) {
+      for (let j = 0; j < a.length - 1 - i*2; j++) {
+          let holder = a[i][j + i];
+          
+          a[i][j + i] = a[a.length - 1 - j - i][i];
+          a[a.length - 1 - j - i][i] = a[a.length - 1 - i][a.length - 1 - i - j];
+          a[a.length - 1 - i][a.length - 1 - i - j] = a[i + j][a.length - 1 - i];
+          a[i + j][a.length - 1 - i] = holder;
       }
-    }
   }
-
-  for (let a = 0; a < a.length; a++) {
-
-  }
-
   return a;
 }
