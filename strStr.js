922827c57304f7e80ef461d3295bb2be65fc24c7
sakwok <s3.kwok@gmail.com>
2019-01-06 02:00:36 -0800

    strstr

---
+++ strStr.js
@@ -0,0 +1,12 @@
+var strStr = function (haystack, needle) {
+  if (needle === "") {
+    return 0;
+  }
+
+  for (let i = 0; i < haystack.length; i++) {
+    if (haystack.slice(i, i + needle.length) === needle) {
+      return i;
+    }
+  }
+  return -1;
+};
